b567153c712a40cf0526c87c97f3669efcd061fa
Keb <104720524+Kicskeb@users.noreply.github.com>
2023-03-26 21:56:46 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <array>
+#include <random>
+#include <set>
+
+class Table {
+private:
+    std::array<char, 25> arr = {};
+public:
+    Table() {
+        std::random_device r;
+        std::default_random_engine gen(r());
+        std::uniform_int_distribution<size_t> dist(0, 24);
+        std::uniform_int_distribution<size_t> dist_2(0, 6);
+        std::set<unsigned int> things;
+        std::set<unsigned int>::iterator it;
+        unsigned int x1=dist_2(gen), x2=x1;
+        int i=0;
+        while (x1 == x2) {
+            x2 = dist_2(gen);
+        };
+        while (things.size() < 7) {
+            things.insert(dist(gen));
+        };
+        for (it = things.begin(); it != things.end(); ++it){
+            arr[*it] = (i == x1 || i == x2) ? 'X' : 'G';
+            i++;
+        };
+    };
+    void printer() {
+        for (auto i : arr) {
+            std::cout << i << " ";
+        };
+        std::cout << std::endl;
+    }
+    void play() {
+        int g_szam = 0, steps = 0;
+        for (auto i : arr) {
+            if (i == 'X') steps += 3;
+            if (i == 'G') g_szam++;
+            steps++;
+            if (g_szam == 5) break;
+        };
+        std::cout << "Required steps: " << steps ;
+    }
+};
+
+int main() {
+    Table jatek;
+//    jatek.printer();
+    jatek.play();
+    return 0;
+}
